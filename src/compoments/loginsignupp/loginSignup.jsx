a992342ace90b863b61ad176e95315cdf005d226
Salomey Boafo <salomeyyboafo@gmail.com>
2025-11-04 15:25:43 +0000

    added mee

--- src/compoments/loginsignupp/loginSignup.jsx
+++ src/compoments/loginsignupp/loginSignup.jsx
@@ -8,7 +8,7 @@ const LoginSignup = () => {
   return (
     <div className='container'>
         <div className="header">
-            <div className="text">Sign Up</div>
+            <div className="text">Sign Up to mee</div>
             <div className='"underline'></div>
         </div>
        
