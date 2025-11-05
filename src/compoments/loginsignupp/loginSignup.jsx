dc45b3100a9e5525bc7bf4e83a1389c00a33819c
Salomey Boafo <salomeyyboafo@gmail.com>
2025-11-05 12:35:16 +0000

    Done

--- src/compoments/loginsignupp/loginSignup.jsx
+++ src/compoments/loginsignupp/loginSignup.jsx
@@ -1,37 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './loginsignup.css'
 import user_icon from'../Assets/person.png'
 import email_icon from'../Assets/email.png'
 import password_icon from'../Assets/password.png'
 
 const LoginSignup = () => {
+
+    const [action,setAction] = useState("Sign Up")
+
   return (
     <div className='container'>
         <div className="header">
-            <div className="text">Sign Up to mee</div>
+            <div className="text">{action}</div>
             <div className='"underline'></div>
         </div>
        
             <div className="inputs">
-            <div className='input'>
+              {action==="Login"?<div></div>:<div className='input'>
                 <img src= {user_icon}  alt="" />
-                <input type="text"/>
+                <input type="text"placeholder="Name" />
 
-            </div>
+            </div>}
+
+
+            
             <div className='input'>
                 <img src={email_icon} alt=""/>
-                <input type="email" />
+                <input type="email" placeholder='Email' />
 
             </div>
             <div className='input'>
                 <img src= {password_icon} alt=""/>
-                <input type="password" />
+                <input type="password" placeholder='Password' />
 
             </div>
         </div>
+        {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost Password?<span>Click Here!</span></div>}
+        
+         <div className="submit-container">
+            <div className={action==="Login"?"submit gray":"submit"}onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+            <div className={action==="Sign Up"?"submit gray":"submit"}onClick={()=>{setAction("Login")}}>Log In</div>
 
-    </div>
+            </div>
+         </div>
+
+
+    
   )
 }
 
-export default LoginSignup
+export default LoginSignup;
